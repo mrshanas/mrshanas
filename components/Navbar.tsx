5b325c61b1f2f9de278074bf2d6ea5224d6749c2
shanas <87828025+mrshanas@users.noreply.github.com>
2022-05-21 02:33:38 +0300

    Responsive nav
    It is responsive but has a bug of color change

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,12 +5,22 @@ import { navLinks } from "@/constants/nav";
 import "hamburgers/dist/hamburgers.css";
 
 const Navbar: React.FC = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const openNav = () => setIsNavOpen((prev) => !prev);
+
   return (
-    <nav className="flex justify-around mx-auto my-5">
+    <nav className="flex flex-row-reverse md:flex-row justify-around mx-auto my-5">
       <div className="md:hidden block my-[-0.8rem]">
-        <button className="hamburger hamburger--collapse" type="button">
+        <button
+          className={`hamburger hamburger--collapse ${
+            isNavOpen ? "is-active" : ""
+          }`}
+          type="button"
+          onClick={openNav}
+        >
           <span className="hamburger-box">
-            <span className="hamburger-inner text-white"></span>
+            <span className="hamburger-inner"></span>
           </span>
         </button>
       </div>
@@ -27,15 +37,15 @@ const Navbar: React.FC = () => {
       </div>
       {/* icons tab */}
       <div className="flex justify-evenly">
-        <div className="mx-3 text-2xl">
+        <a className="mx-3 text-2xl" href="https://github.com/mrshanas">
           <FaGithub className="text-white" />
-        </div>
-        <div className="mx-3 text-2xl">
+        </a>
+        <a className="mx-3 text-2xl">
           <FaLinkedin className="text-white" />
-        </div>
-        <div className="mx-3 text-2xl">
+        </a>
+        <a className="mx-3 text-2xl" href="https://twitter.com/mrshanas">
           <FaTwitter className="text-white" />
-        </div>
+        </a>
       </div>
     </nav>
   );
