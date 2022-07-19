89cf8d4bc34d31c568b1e311d54d8929475fa093
shanas <87828025+mrshanas@users.noreply.github.com>
2022-07-19 04:23:12 +0300

    New UI

---
+++ pages/contact.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { Navbar } from "../components";
+
+const Contact: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Contact mrshanas</title>
+      </Head>
+      <Navbar />
+    </>
+  );
+};
+
+export default Contact;
---
+++ pages/projects.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { Navbar } from "../components";
+
+const Projects: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>mrshanas | Projects</title>
+      </Head>
+      <Navbar />
+    </>
+  );
+};
+
+export default Projects;
