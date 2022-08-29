fcdb314a7aadfbe6da293e416d5f4edad690c488
Shanas <87828025+mrshanas@users.noreply.github.com>
2022-08-30 02:44:25 +0300

    Update projects.ts

--- utils/projects.ts
+++ utils/projects.ts
@@ -15,14 +15,14 @@ export const projects: projects[] = [
     name: "Smartshop",
     tags: ["Django", "Postgres", "Js"],
     desc: "This is an application that allows small scale shop owners to manage their shops by recording transactions, generating receipts and register new items in a shop",
-    link: "https://pos-smartshop.herokuapp.com",
+    link: "https://smartshop.up.railway.app",
     img: smartshop,
   },
   {
     name: "Topics Tracker",
     tags: ["Django", "Bootstrap", "Heroku"],
     desc: "A to-do like app that enables you to register specific topics learnt in a lecture, I made this project when I was learning Django web framework",
-    link: "https://topics-tracker.herokuapp.com",
+    link: "https://github.com/mrshanas/topics-tracker",
     img: topics,
   },
 ];
