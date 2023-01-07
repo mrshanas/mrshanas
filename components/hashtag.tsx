b40cef3f5a142b12261aa3786c1c700a79cf1b6a
Shanas <87828025+mrshanas@users.noreply.github.com>
2023-01-07 20:59:01 +0300

    Delete components directory

--- components/hashtag.tsx
+++
@@ -1,5 +0,0 @@
-import { FC } from "react";
-
-const HashTag: FC = () => <span className="text-primary mr-1">#</span>;
-
-export default HashTag
--- components/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as Footer } from "./footer";
-export { default as HashTag } from "./hashtag";
-export { default as LoadingAnim } from "./loading-anim";
-export { default as Navbar } from "./navbar";
-export { default as PageHeader } from "./page-header";
-export { default as ProjectCard } from "./project-card";
-export { default as SectionHeader } from "./section-header";
-export { default as SkillCard } from "./skill-card";
